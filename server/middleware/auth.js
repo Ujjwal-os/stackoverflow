397a1034085d3d6e95396ba3f543a187fc0423b1
darkPIrate <chaudharyujjwal302@gmail.com>
2022-05-20 15:55:13 +0530

    project-complete

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,10 +3,10 @@ import jwt from 'jsonwebtoken'
 const auth = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]
-
+        
         let decodeData = jwt.verify(token, process.env.JWT_SECRET)
         req.userId = decodeData?.id 
-
+        console.log("i am verifying token...")
         next()
     } catch (error) {
         console.log(error)
